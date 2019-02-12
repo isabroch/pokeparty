cfbea2d67389b3671de64fed9451af4ac545e809
Isabella Broch <isab7099@next-rebb110-19.local>
2019-02-12 16:52:53 +0100

    Initializing

---
+++ script.js
@@ -0,0 +1,18 @@
+var pokemon = [
+  ['Name', 'Species', 'Sex', 'Level', 'Link', 'Type', 'Delta Typing', 'Held Item', 'Pokeball', 'Ability', [
+    ['Move1', 'Move1Type', 'Move1Cat'],
+    ['Move2', 'Move2Type', 'Move2Cat'],
+    ['Move3', 'Move3Type', 'Move3Cat'],
+    ['Move4', 'Move4Type', 'Move4Cat']
+  ]],
+  ['Bitty', 'Mimikyu', 'M', '5', '15', ' :ghost: :fairy:', '-', '-', '-', 'Disguise', [
+    ['Copycat', 'Normal', 'Status'],
+    ['Astonish', 'Ghost', 'Physical'],
+    ['Scratch', 'Normal', 'Physical'],
+    ['Leer', 'Normal', 'Status']
+  ]]
+]
+
+pokemonParty() {
+
+}
